6659b0941404990d6f899070306ede275187a1ad
= <=>
2020-10-06 15:35:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/app_fitness/Clientes_act.java
@@ -0,0 +1,13 @@
+package com.example.app_fitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Clientes_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clientes_act);
+    }
+}
